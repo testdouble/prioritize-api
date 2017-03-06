dea20a37ce8cbf4c5d1f2b1446a764946b0b34d4
Todd Kaufman <toddkaufman@gmail.com>
2017-03-06 16:28:22 -0500

    Added api method for topics

--- web/views/topic_view.ex
+++ web/views/topic_view.ex
@@ -1,3 +1,14 @@
 defmodule PrioritizeApi.TopicView do
   use PrioritizeApi.Web, :view
+
+  def render("topics.json", %{topics: topics}) do
+    %{data: render_many(topics, PrioritizeApi.TopicView, "topic.json")}
+  end
+
+  def render("topic.json", %{topic: topic}) do
+    %{id: topic.id,
+      title: topic.title,
+      description: topic.description,
+      votes: topic.votes}
+  end
 end
